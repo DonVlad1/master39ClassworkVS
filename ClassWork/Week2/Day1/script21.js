e8cd70cca7fdf11a0a680a57aaa5b83dae15e9c8
Vlad <vladut_stoian@live.co.uk>
2022-06-13 10:13:33 +0100

    Added new entruies/files/folders

---
+++ ClassWork/Week2/Day1/script21.js
@@ -0,0 +1,13 @@
+function writeBackwards()
+{
+    let reversedWord = ""
+    for (let letterIndex = 0; letterIndex < word.length; letterIndex++) {
+        
+        console.log("Done")
+    }
+}
+
+
+
+let word = "Cookie"
+writeBackwards(word)
