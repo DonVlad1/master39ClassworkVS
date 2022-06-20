fa61769c1abe020c4afb5b1a63ed097ac0e5ac12
Vlad <vladut_stoian@live.co.uk>
2022-06-20 16:49:21 +0100

    Update

--- ClassWork/Week3/Day1/pt1/7 - Styling Elements with JS/app.js
+++ ClassWork/Week3/Day1/pt1/7 - Styling Elements with JS/app.js
@@ -1,1 +1,15 @@
+const image = document.getElementById("cat");
+const button = document.getElementById("submit");
+
+button.addEventListener("click", function()
+{
+    if (image.hidden == false)
+    {
+        image.hidden = true
+    }
+    else
+    {
+        image.hidden = false        
+    }
+})
 
