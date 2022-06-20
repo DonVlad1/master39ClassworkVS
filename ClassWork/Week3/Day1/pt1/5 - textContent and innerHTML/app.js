ae427d8bbe1c79dab45c12b6f05f467d85f16e41
Vlad <vladut_stoian@live.co.uk>
2022-06-20 14:16:18 +0100

    Adding to the list

---
+++ ClassWork/Week3/Day1/pt1/5 - textContent and innerHTML/app.js
@@ -0,0 +1,12 @@
+const placeholder = document.getElementById("placeholder");
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+const list = document.getElementById("list");
+
+submit.addEventListener("click", function()
+{
+    placeholder.style.color = "goldenrod"
+    placeholder.textContent = `<li>${input.value}</li>`
+
+    list.innerHTML  `<li>${input.value}</li>`
+})
