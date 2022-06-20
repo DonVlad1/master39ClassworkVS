bc16b217c09b8343e29fe345d38401f33f9e583c
Vlad <vladut_stoian@live.co.uk>
2022-06-20 11:46:11 +0100

    Added a loop

---
+++ ClassWork/Week3/Day1/pt1/3 - Selecting Elements of Particular Type/app.js
@@ -0,0 +1,7 @@
+// const drinks = document.getElementById("drinks")
+const list = document.getElementsByTagName("li")
+
+
+for (let index = 0; index < list.length; index++) {
+    console.log(list[index])
+}
