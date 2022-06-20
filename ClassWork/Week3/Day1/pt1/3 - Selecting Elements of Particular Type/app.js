1f30fa551d5984cc32567e8c1b6ba38c6aa7de49
Vlad <vladut_stoian@live.co.uk>
2022-06-20 12:03:38 +0100

    Update

--- ClassWork/Week3/Day1/pt1/3 - Selecting Elements of Particular Type/app.js
+++ ClassWork/Week3/Day1/pt1/3 - Selecting Elements of Particular Type/app.js
@@ -1,6 +1,13 @@
 // const drinks = document.getElementById("drinks")
-const list = document.getElementsByTagName("li")
+// const list = document.getElementsByTagName("li")
 
+
+// for (let index = 0; index < list.length; index++) {
+//     list[index].style.color = "red"
+// }
+
+
+const list = document.getElementsByClassName("not-orange")
 
 for (let index = 0; index < list.length; index++) {
     list[index].style.color = "red"
