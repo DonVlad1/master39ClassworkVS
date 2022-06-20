173e4d9dc6d93a3375360524467679bb08a19160
Vlad <vladut_stoian@live.co.uk>
2022-06-20 15:51:52 +0100

    Update

--- ClassWork/Week3/Day1/pt1/6 - Changing Element Attributes/app.js
+++ ClassWork/Week3/Day1/pt1/6 - Changing Element Attributes/app.js
@@ -1,1 +1,7 @@
+const input = document.getElementById("input");
+const button = document.getElementById("submit");
 
+button.addEventListener("click", function()
+{
+    image.src = input.value;
+});
