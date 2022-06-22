203c30dcf8d43e4da24b9d04b82f7d6db08fd0c6
Vlad <vladut_stoian@live.co.uk>
2022-06-22 13:19:15 +0100

    Added all the sounds, they can be run on keypress

--- ClassWork/Week3/Day3/app.js
+++ ClassWork/Week3/Day3/app.js
@@ -4,15 +4,63 @@ const removeButton = document.getElementById("remove")
 const keyPress = document.getElementById("keyPress")
 const keyCode = document.getElementById("keyCode")
 const keyWhich = document.getElementById("keyWhich")
-const kickSound = document.getElementById("kick")
+const boomkSound = document.getElementById("boom")
+const clapSound = document.getElementById("clap")
+const hiHatSound = document.getElementById("hiHat")
+const kickound = document.getElementById("kick")
+const openHatSound = document.getElementById("openHat")
+const rideSound = document.getElementById("ride")
+const snareSound = document.getElementById("snare")
+const tinkSound = document.getElementById("tink")
+const tomSound = document.getElementById("tom")
 
 // boomSound.play()
 
 button.addEventListener("keypress", (event) => {
     keyPress.textContent = event.key
+    if (event.key == "q")
+    {
+        boomkSound.play()
+    }
+
     if (event.key == "w")
     {
-        kickSound.play()
+        clapSound.play()
+    }
+
+    if (event.key == "e")
+    {
+        hiHatSound.play()
+    }
+
+    if (event.key == "r")
+    {
+        kickound.play()
+    }
+
+    if (event.key == "t")
+    {
+        openHatSound.play()
+    }
+
+    if (event.key == "y")
+    {
+        rideSound.play()
+    }
+
+    if (event.key == "u")
+    {
+        snareSound.play()
+    }
+
+    if (event.key == "i")
+    {
+        tinkSound.play()
+    }
+
+    if (event.key == "o")
+    {
+        tomSound.play()
     }
 
 
