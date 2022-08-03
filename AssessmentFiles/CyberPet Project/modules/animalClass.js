14d93e51980b6b5347a23b877af80ec5d05e7d20
Vlad <vladut_stoian@live.co.uk>
2022-08-03 14:50:34 +0100

    Added play to mainClass

--- AssessmentFiles/CyberPet Project/modules/animalClass.js
+++ AssessmentFiles/CyberPet Project/modules/animalClass.js
@@ -7,19 +7,30 @@ class Animal
         this.tiredness = tiredness
         this.happiness = happiness
     }
+
     eat()
     {
         this.hunger += 60
         this.thirst -= 20
     }
+
     sleep()
     {
         this.tiredness += 80
         this.hunger -= 40
         this.thirst -= 40
     }
+
     drink()
     {
         this.thirst += 60
+    }
+
+    play()
+    {
+        this.happiness += 30
+        this.hunger -= 30
+        this.thirst -= 30
+        this.tiredness -= 30
     }
 }
