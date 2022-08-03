65d8db877cd0d61cb7851d10d01b61b3268ae52b
Vlad <vladut_stoian@live.co.uk>
2022-08-03 14:48:14 +0100

    Made initial basic class

---
+++ AssessmentFiles/CyberPet Project/modules/animalClass.js
@@ -0,0 +1,25 @@
+class Animal
+{   constructor(name, hunger, thirst, tiredness, happiness)
+    {
+        this.name = name
+        this.hunger = hunger
+        this.thirst = thirst
+        this.tiredness = tiredness
+        this.happiness = happiness
+    }
+    eat()
+    {
+        this.hunger += 60
+        this.thirst -= 20
+    }
+    sleep()
+    {
+        this.tiredness += 80
+        this.hunger -= 40
+        this.thirst -= 40
+    }
+    drink()
+    {
+        this.thirst += 60
+    }
+}
