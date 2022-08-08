fe2f30b67646e1b4513f7ce626227c656bab1d10
Vlad <vladut_stoian@live.co.uk>
2022-08-08 16:29:53 +0100

    Completed task

--- ClassWork/Week5/learning-react/my-first-app/src/App.js
+++ ClassWork/Week5/learning-react/my-first-app/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import cadia from "./images/CadianUniform.png"
+import krieg from "./images/KriegUniform.png"
+import steel from "./images/SteelLegionUniform.png"
+import vostroya from "./images/VostroyanUniform.png"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const guardList = [cadia, krieg, steel, vostroya]
+
+function App() 
+{
+    return (
+    <div class="imperialGuardSpace">
+    <AllPictures name = "Hans" unitList = {guardList}/>
     </div>
   );
+  }
+
+function AllPictures(props)
+{
+  return(
+    <div class="guardImages">
+      <Cadia unitList = {guardList[0]}/>
+      <Krieg unitList = {guardList[1]}/>
+      <Steel unitList = {guardList[2]}/>
+      <Vostroya unitList = {guardList[3]}/>
+    </div>
+  )
 }
 
+function Cadia(props)
+{
+  return(
+    <div>
+          <img id = "cadia" src={props.unitList} alt=""/>
+          <p>Renowned for having the most elite units in the Imperium</p>
+    </div>
+  )
+}
+
+function Krieg(props)
+{
+  return(
+    <div>
+      <img id = "krieg" src={props.unitList} alt=""/>
+      <p>Renowned for their disregard for personal safety</p>
+    </div>
+  )
+}
+
+function Steel(props)
+{
+  return(
+    <div>
+      <img id = "steel" src={props.unitList} alt=""/>
+      <p>Renowned for thir extensive use of mechanised warfare</p>
+    </div>
+
+  )
+}
+
+function Vostroya(props)
+{
+  return(
+    <div>
+      <img id = "vostroya" src={props.unitList} alt=""/>
+      <p>Renowned for their bear cavalry</p>
+    </div>
+  )
+}
+
+
 export default App;
+
