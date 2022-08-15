4093e18c89d3df1c22637ea975c2f2113a155bea
Vlad <vladut_stoian@live.co.uk>
2022-08-15 17:33:55 +0100

    Use oline resources to make a simple modal function

--- ClassWork/Week6/fetch-data/src/App.js
+++ ClassWork/Week6/fetch-data/src/App.js
@@ -1,50 +1,72 @@
 import React, { useState, useEffect } from "react";
-import Modal from 'react-modal';
+// import Modal from 'react-modal';
+import modalModule from './Modal'
 import './App.css';
-
+import Modal from "./Modal";
 
 const App = () => {
-  const [errorMsg, setErrorMsg] = useState('')
-  const [anime, setAnime] = useState([])
 
-  useEffect (() => {
-    const fetchData = async () => {
+const [show, setShow] = useState(false)
 
-      try {
-        setErrorMsg('')
-        const response = await fetch('https://anime-facts-rest-api.herokuapp.com/api/v1');
-        if(!response.ok){
-          throw new Error(response.statusText)
-        }
-        const data = await response.json();
-        console.log(response)
-        console.log(data)
-        setAnime(data.data)
-      } catch (error) {
-        setErrorMsg("Oops, something went wrong...")
-        console.log(error.message)
-      }
-    }
-    fetchData()
-},[]);
+return(
+  <div className='App'>
+    <button onClick={() => setShow(true)}>Show Modal</button>
+    <Modal onClose={() => setShow(false)} show = {show}/>
+  </div>
+)
 
-  if(errorMsg !== ''){
-    return <h1>{errorMsg}</h1>
-  }
+//   const [errorMsg, setErrorMsg] = useState('')
+//   const [anime, setAnime] = useState([])
 
-  return (
-    <div class='imageContainer'>
-      <h1>Fetch API</h1>
-      {anime.map((animeList, index) => {
-        return (
-          <div class='animeImages' key={index}>
-            <img src={animeList.anime_img} alt="movie poster"/>
-            <p>{animeList.anime_name}</p>
-          </div>
-        )
-      })}
-    </div>
-  );
+
+
+//   function testFunction()
+//   {
+//     console.log("testMe")
+//   }
+
+
+//   useEffect (() => {
+//     const fetchData = async () => {
+
+//       try {
+//         setErrorMsg('')
+//         const response = await fetch('https://anime-facts-rest-api.herokuapp.com/api/v1');
+//         if(!response.ok){
+//           throw new Error(response.statusText)
+//         }
+//         const data = await response.json();
+//         console.log(response)
+//         console.log(data)
+//         setAnime(data.data)
+//       } catch (error) {
+//         setErrorMsg("Oops, something went wrong...")
+//         console.log(error.message)
+//       }
+//     }
+//     fetchData()
+// },[]);
+
+//   if(errorMsg !== ''){
+//     return <h1>{errorMsg}</h1>
+//   }
+
+//   return (
+//     <div class='imageContainer'>
+//       {anime.map((animeList, index) => {
+//         return (
+//           <div class='animeImages' key={index}>
+//             <img onClick= {testFunction} src={animeList.anime_img} alt="movie poster"/>
+//             <p>{animeList.anime_name}</p>
+//           </div>
+//         )
+//       })}
+//     </div>
+//   );
+// }
+
+
+
+
 }
-
 export default App;
