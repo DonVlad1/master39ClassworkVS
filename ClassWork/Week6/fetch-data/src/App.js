799f85b13de5f3b6a2388b4ce00fcce94b00ec1a
Vlad <vladut_stoian@live.co.uk>
2022-08-15 13:48:32 +0100

    Working Map through API data

--- ClassWork/Week6/fetch-data/src/App.js
+++ ClassWork/Week6/fetch-data/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function App() 
-{
-  const[toggle, setToggle] = useState(false)
 
-  useEffect(function(){
-    console.log('component created')
-  })
+const App = () => {
+  const [errorMsg, setErrorMsg] = useState('')
+  const [anime, setAnime] = useState([])
 
-  return(
+  useEffect (() => {
+    const fetchData = async () => {
+
+      try {
+        setErrorMsg('')
+        const response = await fetch('https://anime-facts-rest-api.herokuapp.com/api/v1');
+        if(!response.ok){
+          throw new Error(response.statusText)
+        }
+        const data = await response.json();
+        console.log(response)
+        console.log(data)
+        setAnime(data.data)
+      } catch (error) {
+        setErrorMsg("Oops, something went wrong...")
+        console.log(error.message)
+      }
+    }
+    fetchData()
+},[]);
+
+  if(errorMsg !== ''){
+    return <h1>{errorMsg}</h1>
+  }
+
+  return (
     <div>
       <h1>Fetch API</h1>
-      <button onClick={() => setToggle(!toggle)}>toggle</button>
+      {anime.map((animeList, index) => {
+        return (
+          <div key={index}>
+            <img src={animeList.anime_img} alt="movie poster"/>
+            <p>{animeList.anime_name}</p>
+          </div>
+        )
+      })}
     </div>
-  )
-
-
+  );
 }
 
 export default App;
