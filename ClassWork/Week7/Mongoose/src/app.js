6a6e043474c06c53ab84230fd1c076abcc2cb2e5
Vlad <vladut_stoian@live.co.uk>
2022-08-24 14:11:25 +0100

    Update, FindActor, FindTitle added and working

--- ClassWork/Week7/Mongoose/src/app.js
+++ ClassWork/Week7/Mongoose/src/app.js
@@ -2,7 +2,7 @@ require('./db/connnections');
 const mongoose = require('mongoose')
 const yargs = require('yargs')
 
-const { createMovie, listMovies } = require('./movie/movieFunctions')
+const { createMovie, listMovies, deleteMovie, updateMovie, findActor, findTitle } = require('./movie/movieFunctions')
 
 async function App(yargsObject)
 {
@@ -17,8 +17,25 @@ async function App(yargsObject)
         }
         else if (yargsObject.list)
         {
-            //
             console.log(await listMovies())
+        }
+        else if (yargsObject.delete)
+        {
+            await deleteMovie({ title: yargsObject.title, actor: yargsObject.actor })
+            console.log(await listMovies())
+        }
+        else if (yargsObject.update)
+        {
+            await updateMovie({ title: yargsObject.title, actor: yargsObject.actor, titleR: yargsObject.titleR, actorR: yargsObject.actorR })
+            console.log(await listMovies())
+        }
+        else if (yargsObject.findAllActors) 
+        {
+            await findActor({ actor: yargsObject.actor })
+        }
+        else if (yargsObject.findOneTitle)
+        {
+            await findTitle({ title: yargsObject.title })
         }
         else
         {
--- ClassWork/Week7/Mongoose/src/movie/movieFunctions.js
+++ ClassWork/Week7/Mongoose/src/movie/movieFunctions.js
@@ -23,3 +23,52 @@ exports.listMovies = async function ()
 
     }
 }
+
+exports.deleteMovie = async function (movieObject)
+{
+    try
+    {
+        return await Movie.deleteOne(movieObject)
+
+    } catch (error)
+    {
+
+    }
+}
+
+exports.updateMovie = async function (movieObject)
+{
+    try
+    {
+        return await Movie.updateOne({ title: movieObject.titleR, actor: movieObject.actorR })
+
+    } catch (error)
+    {
+
+    }
+}
+
+exports.findActor = async function (movieObject)
+{
+    try
+    {
+        return console.log(await Movie.find(movieObject))
+    }
+    catch (error)
+    {
+
+    }
+}
+
+exports.findTitle = async function (movieObject)
+{
+    try
+    {
+        return console.log(await Movie.find(movieObject))
+    }
+    catch (error)
+    {
+
+    }
+}
+
